09c58d0affad5719870437250845fa9786dafc95
Ayden <Theownage79@gmail.com>
2024-08-11 13:10:48 -0600

    inventory fixes, many fixes

--- Assets/Scripts/Handlers/Goal.cs
+++ Assets/Scripts/Handlers/Goal.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using DefaultNamespace;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -11,6 +12,7 @@ public class NewBehaviourScript : MonoBehaviour
     [SerializeField] private GameObject _nextLevel;
     [SerializeField] private GameObject _currentLevel;
     private PlayerController2D _playerController2D;
+    [SerializeField] private InventoryController _inventoryController;
 
     private void Start()
     {
@@ -29,8 +31,12 @@ public class NewBehaviourScript : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            _playerController2D.RecalculateHealth();
-            NextLevel();
+            Debug.Log(_inventoryController._inventory.Items[ItemType.Fuse].Amount);
+            if (_inventoryController._inventory.Items[ItemType.Fuse].Amount > 0)
+            {
+                _playerController2D.RecalculateHealth();
+                NextLevel();
+            }
         }
     }
 
--- Assets/Scripts/Inventory/Inventory.cs
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -6,44 +6,34 @@ using UnityEngine;
 public enum ItemType{
     Points, Spinach, Fuse, Pliers,
 }
+
 namespace DefaultNamespace
 {
     [CreateAssetMenu(fileName = "Inventory", menuName = "SOs/Inventory", order = 1)]
-    public class Inventory: ScriptableObject
+    public class Inventory : ScriptableObject
     {
         public Dictionary<ItemType, ItemModel> Items { get; private set; } = new Dictionary<ItemType, ItemModel>();
         public event Action<ItemType> ItemCollectedEvent;
+
+        public InventoryController InventoryController;
 
         private void OnEnable()
         {
             
         }
 
-        public void ItemCollected(ItemType type)
+        public void ItemCollected(ItemType type, ItemModel model)
         {
+            Items.TryAdd(type,model);
+            InventoryController.ItemCollected(type);
             ItemCollectedEvent?.Invoke(type);
+            
             Debug.Log(type);
-        }        
-    }
-
-    public class InventoryController : MonoBehaviour
-    {
-        [SerializeField] private Inventory _inventory;
-        [SerializeField] private InventoryView _view;
-
-        void Start()
-        {
-            _inventory.ItemCollectedEvent += ItemCollected;
         }
 
-        private void ItemCollected(ItemType itemType)
+        public void AddItemAmount()
         {
-            _inventory.Items[itemType].Amount++;
+            
         }
-    }
-
-    public class InventoryView: MonoBehaviour
-    {
-        
     }
 }
